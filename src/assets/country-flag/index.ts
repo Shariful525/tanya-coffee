89370652164f435153bf1d584c3038b9548e1b64
poran120 <mrporanyt120@gmail.com>
2025-10-06 11:16:54 +0600

    add(country): added country assets

---
+++ src/assets/country-flag/index.ts
@@ -0,0 +1,24 @@
+import malaysiaFlag from "../country-flag/malaysia.png";
+import canadaFlag from "../country-flag/canada.png";
+import hongkongFlag from "../country-flag/hong-kong.png";
+import indiaFlag from "../country-flag/india.png";
+import indonesiaFlag from "../country-flag/indonesia.png";
+import newzealandFlag from "../country-flag/new-zealand.png";
+import pakistanFlag from "../country-flag/pakistan.png";
+import singaporeFlag from "../country-flag/singapore.png";
+import taiwanFlag from "../country-flag/taiwan.png";
+import uaeFlag from "../country-flag/uae.png";
+
+const countryFlags = {
+  malaysiaFlag,
+  canadaFlag,
+  hongkongFlag,
+  indiaFlag,
+  indonesiaFlag,
+  newzealandFlag,
+  pakistanFlag,
+  singaporeFlag,
+  taiwanFlag,
+  uaeFlag,
+};
+export default countryFlags;
